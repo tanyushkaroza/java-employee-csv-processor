120ca171899e46525944530dd0bab010f5221c89
tanyushkaroza <tanaroza377@gmail.com>
2025-12-09 15:48:44 +0300

    feat(Division.java): create division entity with auto-increment ID *Implement Division class with id and name fields *Add static nextId counter for automatic ID generation *Create equals/hashCode based on id field *Implement toString() method for division representation *Add default and parameterized constructors

---
+++ working_with_csv_file/src/main/java/employee/Division.java
@@ -0,0 +1,57 @@
+package employee;
+import java.util.Objects;
+
+/**
+ * Сущность "Division", которая представляет собой подразделения.
+ */
+public class Division {
+    private Long id;
+    private String name;
+    private static long nextId = 1;
+
+    public Division() {
+        this.id = nextId++;
+    }
+
+    public Division(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Division division = (Division) o;
+        return Objects.equals(id, division.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Division{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
